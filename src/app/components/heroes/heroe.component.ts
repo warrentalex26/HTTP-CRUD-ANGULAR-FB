77978e55347e1bef12ee2b78eafdfa755908621e
warrentalex26 <warrentalex26@hotmail.com>
2019-02-26 10:49:52 -0600

    PUT, GET realizados con comentarios

--- src/app/components/heroes/heroe.component.ts
+++ src/app/components/heroes/heroe.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { HeroeInterface } from '../../interface/heroe.interface';
 import { HeroesService } from '../../services/heroes.service';
 
@@ -17,18 +17,71 @@ export class HeroeComponent implements OnInit {
     casa: 'Marvel'
   };
 
+  //una bandera que utilizaremos para ver si es un nuevo registro o una actualizacion
+  id:boolean =false;
+
+  //le llame llave para diferenciarlo de id y que sea claro
+  key:string;
+
   constructor(private _heroesService: HeroesService,
-              private _router: Router) { }
+              private _router: Router,
+              private _activatedRoute: ActivatedRoute) { 
+    
+    //Para obtener la URL y con eso sacamos la llave osea el ID para la actualización
+    this._activatedRoute.params.subscribe(parametros => {
+      console.log(parametros);
+      //el id que viene de los parametros debe ser el mismo que declaramos en "app-routing.module.ts" es decir id
+      this.key = parametros['id'];
+
+      //Determinamos si cargamos los datos o no
+      if (this.key !== 'id'){ //si es diferente de id osea que ya hay una llave en la URL entonces se cargan los datos
+        this._heroesService.getHeroe(this.key)
+          //Nos subscribimos para poder utiliza el servicio
+          .subscribe(data => {
+            //this.heroe es el que esta declarado en la parte superior eso lo igualamos a la Data para que cargue los datos almacenados de firebase en nuestro formulario
+            return this.heroe = data
+          })
+      }
+    })
+    
+  }
 
   ngOnInit() {
   }
 
   guardar() {
     console.log(this.heroe);
-    this._heroesService.nuevoHeroe(this.heroe)
-      .subscribe(data => {
-        this._router.navigate(['/heroe', data.name]); //name es el nombre que recibe los datos en firebase
-      },error1 => console.error(error1));
+
+    //Definimos si viene la palabra nuevo para insertar un registro o si viene la llave es una actualizacion del registro
+    if (this.key == 'id') {
+      //Insertando
+      //Usamos el servicio "heroes.service.ts" y le pasamos el heroe
+      this._heroesService.nuevoHeroe(this.heroe)
+      //Se necesita el subscribe para poder guardar la data con el servicio
+        .subscribe(data => {
+          //name es el nombre que recibe los datos en firebase esto para que cambie el "id" por la clave en la URL y navegamos a esa pantall
+          this._router.navigate(['/heroe', data.name]);
+          //Por si pasa un error
+        },error1 => console.error(error1));
+    }else {
+      //Actualizando
+      //Usamos el servicio "heroes.service.ts" y le pasamos el heroe y la llave que estamos agarrando en la URL que lo hicimos en el constructor
+      this._heroesService.actualizarHeroe(this.heroe, this.key)
+      //Se necesita el subscribe para poder guardar la data con el servicio
+        .subscribe(data => {
+          console.log(data);
+          //Por si pasa un error
+        },error1 => console.error(error1));
+    }
+  }
+
+  agregarNuevo(forma: NgForm){
+    //navegamos a una nueva ruta
+    this._router.navigate(['/heroe','id']);
+    //Hacemos el reset de la forma del formulario, excepto la casa.
+    forma.reset({
+      casa: "Marvel"
+    });
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -7,25 +7,65 @@ import { map } from 'rxjs/operators';
 
 export class HeroesService {
 
+  //Le decimos a Angular que este es el URL que se necesita usar para hacer el CRUD
   fireAPI_URL: string = 'https://heroesapp-e4dfc.firebaseio.com/heroes.json';
+  heroe_URL: string = 'https://heroesapp-e4dfc.firebaseio.com/heroes';
 
+  //Inyectamos en el constructor el HttpClient para usarlo en el servicio
   constructor(private http: HttpClient) {
 
   }
 
+  //Creamos el nuevo Heroe, recibe el heroe de tipo heroe para que no recibimos datos que no son los esperados
   nuevoHeroe(heroe: HeroeInterface) {
 
+    //Body es ingual a JSON.Stringify para que cree un JSON con un string completamente VALIDO que usa Firebase
     let body = JSON.stringify(heroe);
+    //Definimos el encabezado de la petición
     let headers = new HttpHeaders({
       'Content-Type': 'application/json'
     });
 
-
+    //Necesitamos que regrese un observable que nos diga si se inserto o no, para eso el http.post lo regresa.
     return this.http.post( this.fireAPI_URL, body, {headers} )
+      //map nos ayuda a trasnformar la data que viene
       .pipe(map(res => {
         console.log(res);
         return res;
       }));
 
   }
+
+  actualizarHeroe(heroe: HeroeInterface, key$: string) {
+
+    //Body es ingual a JSON.Stringify para que cree un JSON con un string completamente VALIDO que usa Firebase
+    let body = JSON.stringify(heroe);
+    //Definimos el encabezado de la petición, es opcional pero esta para que validemos al mandar el header en la solicitud
+    let headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    //obtenemos la url con el nodo a actualizar
+    let url = `${this.heroe_URL}/${key$}.json`;
+
+    //Necesitamos que regrese un observable que nos diga si se inserto o no, para eso el http.post lo regresa.
+    return this.http.put( url, body, {headers} )
+    //map nos ayuda a trasnformar la data que viene
+      .pipe(map(res => {
+        console.log(res);
+        return res;
+      }));
+
+  }
+
+  getHeroe(key$: string){
+    //obtenemos el url
+    let url = `${this.heroe_URL}/${key$}.json`;
+    return this.http.get(url)
+      .pipe(map(res => {
+        console.log(res);
+        return res;
+      }))
+  }
+
 }
